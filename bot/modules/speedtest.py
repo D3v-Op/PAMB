5827aa0205a4eefa3e37854b6a5b6508bc8fe608
MOOGAA <arjunsinhchudasama7.6@gmail.com>
2021-04-26 20:05:20 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -18,7 +18,7 @@ def speedtst(update, context):
     test.results.share()
     result = test.results.dict()
     context.bot.editMessageText(
-        "Speeds ⚡️ : "f"\n\n 📥 {speed_convert(result['download'])}\n 📤 {speed_convert(result['upload'])}\n"
+        "Speeds ⚡️ : "f"\n 📥 {speed_convert(result['download'])}\n 📤 {speed_convert(result['upload'])}\n\n"
         "⏱ Ping : "f"{result['ping']}\n"
         "🏬 ISP : "
         f"{result['client']['isp']}",
